2a49de681f7166901ff90d8452df9b7688ca80fe
Miquéias Fernandes <contatomiqueiasfernandes@hotmail.com>
2017-03-07 14:04:20 -0300

    variavel R_HOmE verificada no inicio

--- src/jriacces2/JRIacces2.java
+++ src/jriacces2/JRIacces2.java
@@ -36,11 +36,30 @@ public class JRIacces2 {
             System.exit(Errors.ARGS_INVALIDO.ordinal());
         }
 
+        String path = System.getenv("R_HOME");
+
+        if (path == null || path.isEmpty() || path.length() < 2) {
+
+            String s = "R_HOME=/usr/lib64/R\n"
+                    + "export R_HOME\n"
+                    + "R_SHARE_DIR=/usr/share/R\n"
+                    + "export R_SHARE_DIR\n"
+                    + "R_INCLUDE_DIR=/usr/include/R\n"
+                    + "export R_INCLUDE_DIR\n"
+                    + "R_DOC_DIR=/usr/share/doc/R\n"
+                    + "export R_DOC_DIR\n"
+                    + "LD_LIBRARY_PATH=/home/mfernandes/R/x86_64-redhat-linux-gnu-library/3.3/rJava/jri\n"
+                    + "export LD_LIBRARY_PATH";
+            System.err.println("Não é possivel iniciar devido às variaveis de ambiente do JRI.\n,"
+                    + "Para solucionar, altere e adicione em ~/.bashrc:\n" + s);
+            System.exit(Errors.SYSTEM_ENVIRONMENT.ordinal());
+        }
+
         ILog log = null;
 
         try {
             log = new TXTLog(args[1], LogType.getLogType(args[2]));
-            log.printLog(LogType.LOG_DEBUG, "Arquivo Log inicializado com sucesso. {JRIaccess2.java/42}");
+            log.printLog(LogType.LOG_DEBUG, "Arquivo Log inicializado com sucesso. {JRIaccess2.java/62}");
             log.printLog(LogType.LOG_INFO, "[ ARGS ]" + Arrays.toString(args));
         } catch (IOException ex) {
             System.err.println("erro ao inicializar arquivo de log: " + ex);
@@ -55,15 +74,15 @@ public class JRIacces2 {
         } else {
             switch (modo) {
                 case "LIVE":
-                    log.printLog(LogType.LOG_DEBUG, "Inicializando protocolo. {JRIaccess2.java/55}");
+                    log.printLog(LogType.LOG_DEBUG, "Inicializando protocolo. {JRIaccess2.java/77}");
                     Protocolo protocolo = new Protocolo(Arrays.copyOfRange(args, 3, args.length), log);
-                    log.printLog(LogType.LOG_DEBUG, "Protocolo inicializado com sucesso. {JRIaccess2.java/57}");
+                    log.printLog(LogType.LOG_DEBUG, "Protocolo inicializado com sucesso. {JRIaccess2.java/79}");
                     break;
                 case "BATCH":
                     log.printLog(LogType.LOG_DEBUG, "inicializando em modo BATCH. {JRIaccess2.java/60}");
                     break;
                 default:
-                    log.printLog(LogType.LOG_DEBUG, "Arquivo Log inicializado com sucesso. {JRIaccess2.java/63}");
+                    log.printLog(LogType.LOG_DEBUG, "Arquivo Log inicializado com sucesso. {JRIaccess2.java/85}");
                     System.out.println("USAGE: [LIVE/BATCH]");
                     System.exit(Errors.ARGS_INVALIDO.ordinal());
             }
